5f2f59841f562461f32234797f691e336b761037
chy131 <chy131@chy131deMacBook-Air.local>
2020-05-05 15:57:51 -0400

    Update perceptron

--- perceptron.py
+++ perceptron.py
@@ -55,7 +55,7 @@ data_df_filtered = remover.transform(data_df_tokenized)
 
 # hashing term frequency 
 hashing_term_freq = \
-    HashingTF(inputCol="words", outputCol="featuresRaw", numFeatures=100)
+    HashingTF(inputCol="words", outputCol="featuresRaw", numFeatures=200)
 data_df_tf = hashing_term_freq.transform(data_df_filtered)
 
 # inverse document frequency
