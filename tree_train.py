5616822e996e11ee3f2b937410587b1674a2ffbe
chy131 <chy131@chy131deMacBook-Air.local>
2020-05-08 23:53:13 -0400

    Update tree train

---
+++ tree_train.py
@@ -0,0 +1,83 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import udf, col, rand
+from pyspark.sql.types import *
+from pyspark.ml import Pipeline
+from pyspark.ml.feature import RegexTokenizer, StopWordsRemover, HashingTF, IDF 
+from pyspark.ml.feature import StringIndexer
+from pyspark.ml.classification import GBTClassifier
+from pyspark.ml.tuning import ParamGridBuilder, TrainValidationSplit
+from pyspark.ml.evaluation import BinaryClassificationEvaluator
+from pyspark.mllib.evaluation import BinaryClassificationMetrics
+from pyspark.sql.functions import rand
+
+# local mode
+"""
+spark = SparkSession \
+    .builder \
+    .appName("IsItHelpfull") \
+    .master("local[*]") \
+    .getOrCreate()
+"""
+
+# cluster mode
+spark = SparkSession \
+    .builder \
+    .appName("IsItHelpfull") \
+    .config("spark.executor.instances", 4) \
+    .config("spark.executor.cores", 2) \
+    .getOrCreate()
+
+def category_review(votes):
+    score = votes[0]/votes[1]
+    return "good" if score >= 0.8 else "else"
+
+category_review_udf = udf(category_review, StringType())
+
+review_df = spark \
+    .read.json("tools.json") \
+    .where(col("helpful")[1] >= 5) \
+    .withColumn("category", category_review_udf("helpful")) \
+    .select("reviewText", "category") \
+    .cache()
+
+# up-sample the else category, so that #else = #good
+review_df_good = review_df.where(col("category") == "good").cache()
+review_df_else = review_df.where(col("category") == "else").cache()
+n_good = review_df_good.count()
+n_else = review_df_else.count()
+fraction = float(n_good)/n_else
+review_df_else_upsampled = \
+    review_df_else.sample(withReplacement=True, fraction=fraction)
+review_df_preprocessed = review_df_good.unionAll(review_df_else_upsampled)
+
+review_df_preprocessed.show()
+
+# tokenize words
+tokenizer = \
+    RegexTokenizer(inputCol="reviewText", outputCol="wordsRaw", pattern="\\W")
+
+# remove stop words
+remover = StopWordsRemover(inputCol="wordsRaw", outputCol="words")
+
+# hashing term frequency 
+hashing_term_freq = \
+    HashingTF(inputCol="words", outputCol="featuresRaw", numFeatures=5000)
+
+# inverse document frequency
+inv_doc_freq = IDF(inputCol="featuresRaw", outputCol="features", minDocFreq=5)
+
+# encode classes
+indexer = StringIndexer(inputCol="category", outputCol="label")
+
+train, test = data_prepared_df.randomSplit([0.8, 0.2], seed=205);
+
+gbt = GBTClassifier(labelCol='label', featuresCol="features", maxIter=500, maxDepth=8)
+pipeline = Pipeline(stages=[tokenizer, remover, hashing_term_freq, inv_doc_freq, indexer, gbt])
+model = pipeline.fit(train)
+
+prediction = model.transform(test)
+evaluator = BinaryClassificationEvaluator(rawPredictionCol='probability', labelCol = 'label')
+AUC = evaluator.evaluate(prediction, {evaluator.metricName: "areaUnderROC"})
+AUP = evaluator.evaluate(prediction, {evaluator.metricName: "areaUnderPR"})
+print("Area under ROC = {}".format(AUC))
+print("Area under PR = {}".format(AUP))
